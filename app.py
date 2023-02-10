aba55c2681a0041136eb1912e66dd94fe1df7a28
poopface-exploded <112532586+poopface-exploded@users.noreply.github.com>
2023-02-09 19:13:14 -0800

    Update app.py

--- app.py
+++ app.py
@@ -17,45 +17,6 @@ server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 server.bind(ADDR)
 
-def receive(conn, addr):
-    connected = True
-    while connected:
-        message_len = conn.recv(HEADER).decode(FORMAT)
-        if message_len:
-            message_len = int(message_len)
-            message = conn.recv(message_len).decode(FORMAT)
-        #print (message)
-        #print(addr)
-        #print(conn)
-        #conn.send("[MESSAGE RECEIVED]".encode(FORMAT))
-        try:
-            messages.put((message, addr, conn))
-        except:
-            print("failed")
-
-def broadcast():
-    while True:
-        while not messages.empty():
-            message, addr, conn = messages.get()
-            #print(message)
-            if conn not in clients:
-                clients.append(conn)
-                print("added!")
-            for client in clients:
-                #print(client)
-                print (addr)
-                client.send(f"<{str(addr)}> {message}".encode(FORMAT))
-                print(message)
-
-def start():
-    server.listen()
-    while True:
-        conn, addr = server.accept()
-        thread = threading.Thread(target = receive, args = (conn, addr))
-        thread.start()
-        thread_brdcst = threading.Thread(target = broadcast)
-        thread_brdcst.start()
-
 app = Flask(__name__)
 
 @app.route('/')
